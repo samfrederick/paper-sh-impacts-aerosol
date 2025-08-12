70752f3752395e279fe534cf6d9c616b773f7482
Sam Frederick <42592395+samfrederick@users.noreply.github.com>
2025-08-12 14:20:44 -0500

    Move code for generating environment into separate bash script

--- 1_submitted_paper/scripts/create_env.sh
+++ 1_submitted_paper/scripts/create_env.sh
@@ -7,6 +7,4 @@
 #source /Users/sf20/opt/anaconda3/etc/profile.d/conda.sh
 source ~/anaconda3/etc/profile.d/conda.sh
 
-conda init bash
-conda activate paper_env
-f2py -c nsh.f90 -m nsh
+conda create -c conda-forge -n paper_env python=3.9 numpy scipy matplotlib netCDF4 pandas ipykernel "setuptools<60" gfortran --yes
