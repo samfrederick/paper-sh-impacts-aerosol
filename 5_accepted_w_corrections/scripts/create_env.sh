15b13d4a44f3620d9f56f73f7665bbf56845f2f6
Sam Frederick <42592395+samfrederick@users.noreply.github.com>
2026-03-26 14:37:09 -0500

    Add revised manuscript for submission

---
+++ 5_accepted_w_corrections/scripts/create_env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Note - enable execution first ('chmod +x compile_nsh.sh') prior to running this script
+
+# source your conda.sh pointed to by the conda base environment
+# https://github.com/conda/conda/issues/7980#issuecomment-441358406
+#source /Users/sf20/opt/anaconda3/etc/profile.d/conda.sh
+source ~/anaconda3/etc/profile.d/conda.sh
+
+conda create -c conda-forge -n paper_env python=3.9 numpy scipy matplotlib netCDF4 pandas ipykernel "setuptools<60" gfortran --yes
